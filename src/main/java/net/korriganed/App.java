bab52282d67931550097f8ecd203346992e04898
chat-wane <grumpy.chat.wane@gmail.com>
2016-11-29 14:17:38 +0100

    initialize maven

---
+++ src/main/java/net/korriganed/App.java
@@ -0,0 +1,13 @@
+package net.korriganed;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
